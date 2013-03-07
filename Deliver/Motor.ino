2db5e1f5eda171a0d822a90f3170fde31e965b16
XTao <xutao881001@gmail.com>
2013-03-07 14:04:50 +0800

    Add new project.

---
+++ Deliver/Motor.ino
@@ -0,0 +1,133 @@
+/*
+ Motor
+ Copyright @ InfiniteSense.com
+*/
+
+/* pwm port 3, 5, 6, 9, 10, 11 */
+#define PIN_PWM_LEFT 11
+#define PIN_DIR1_LEFT 10
+#define PIN_DIR2_LEFT 9
+#define PIN_PWM_RIGHT 6
+#define PIN_DIR1_RIGHT 5
+#define PIN_DIR2_RIGHT 4
+
+#define MOTOR_NUM 2
+#define MOTOR_PWM 0
+#define MOTOR_DIR1 1
+#define MOTOR_DIR2 2
+#define MOTOR_LEFT 0
+#define MOTOR_RIGHT 1
+
+int motor_array[3][MOTOR_NUM] = { 0 };
+long motor_delay = 0; // in us
+const int motor_pin_array[3][MOTOR_NUM] = {
+  {PIN_PWM_LEFT, PIN_PWM_RIGHT}, // digital PWM pins
+  {PIN_DIR1_LEFT, PIN_DIR1_RIGHT},
+  {PIN_DIR2_LEFT, PIN_DIR2_RIGHT}
+};
+
+
+void initMotor()
+{
+  int i;
+  for (i = 0; i < MOTOR_NUM; i++) {
+    pinMode(motor_pin_array[MOTOR_DIR1][i], OUTPUT);
+    pinMode(motor_pin_array[MOTOR_DIR2][i], OUTPUT); 
+  }
+}
+
+void runForward(int speed, int delay)
+{
+  motorForward(MOTOR_LEFT, speed);
+  motorForward(MOTOR_RIGHT, speed);
+  motor_delay = delay;
+}
+
+void runBackward(int speed, int delay)
+{
+  motorBackward(MOTOR_LEFT, speed);
+  motorBackward(MOTOR_RIGHT, speed);
+  motor_delay = delay;
+}
+
+void runLeft(int speed, int delay)
+{
+  motorBackward(MOTOR_LEFT, speed);
+  motorForward(MOTOR_RIGHT, speed);
+  motor_delay = delay;
+}
+
+void runRight(int speed, int delay)
+{
+  motorForward(MOTOR_LEFT, speed);
+  motorBackward(MOTOR_RIGHT, speed);
+  motor_delay = delay;
+}
+
+void stopDead(int delay)
+{
+  motorStop(MOTOR_LEFT);
+  motorStop(MOTOR_RIGHT);
+  motor_delay = delay;
+}
+void motorForward(int motor, int speed)
+{
+  motor_array[MOTOR_PWM][motor] = speed;
+  motor_array[MOTOR_DIR1][motor] = HIGH;
+  motor_array[MOTOR_DIR2][motor] = LOW;
+  //analogWrite(motor_array[MOTOR_PWM][motor], speed);
+  //digitalWrite(motor_array[MOTOR_DIR1][motor], HIGH);
+  //digitalWrite(motor_array[MOTOR_DIR2][motor], LOW);
+}
+
+void motorBackward(int motor, int speed)
+{
+  motor_array[MOTOR_PWM][motor] = speed;
+  motor_array[MOTOR_DIR1][motor] = LOW;
+  motor_array[MOTOR_DIR2][motor] = HIGH;
+  //analogWrite(motor_array[MOTOR_PWM][motor], speed);
+  //digitalWrite(motor_array[MOTOR_DIR1][motor], LOW);
+  //digitalWrite(motor_array[MOTOR_DIR2][motor], HIGH); 
+}
+
+void motorStop(int motor)
+{
+  motor_array[MOTOR_PWM][motor] = 0;
+  motor_array[MOTOR_DIR1][motor] = HIGH;
+  motor_array[MOTOR_DIR2][motor] = HIGH;
+  //analogWrite(motor_array[MOTOR_PWM][motor], 0);
+  //digitalWrite(motor_array[MOTOR_DIR1][motor],HIGH);
+  //digitalWrite(motor_array[MOTOR_DIR2][motor],HIGH);  
+}
+
+int driveMotor()
+{
+  int i;
+  for (i = 0; i < MOTOR_NUM; i++) {
+    analogWrite(motor_pin_array[MOTOR_PWM][i], motor_array[MOTOR_PWM][i]);
+    digitalWrite(motor_pin_array[MOTOR_DIR1][i], motor_array[MOTOR_DIR1][i]);
+    digitalWrite(motor_pin_array[MOTOR_DIR2][i], motor_array[MOTOR_DIR2][i]);
+  }
+  delayMicroseconds(motor_delay);
+}
+
+void runByPosition(int position, int delay)
+{
+  if (position < -5) {
+    motorForward(MOTOR_LEFT, 120);
+    motorForward(MOTOR_RIGHT, 20);
+  } else if (position >= -5 && position < -1) {
+    motorForward(MOTOR_LEFT, 120); 
+    motorForward(MOTOR_RIGHT, 25 * position + 145);
+  } else if (position >= -1 && position <= 1) {
+    motorForward(MOTOR_LEFT, 120);
+    motorForward(MOTOR_RIGHT, 120);
+  } else if (position > 1 && position <= 5) {
+    motorForward(MOTOR_LEFT, -25 * position + 145);
+    motorForward(MOTOR_RIGHT, 120);
+  } else if (position > 5) {
+    motorForward(MOTOR_LEFT, 20);
+    motorForward(MOTOR_RIGHT, 120);
+  }
+  motor_delay = delay;
+}
---
+++ TurnBack/TurnBack.ino
@@ -0,0 +1,162 @@
+/*
+ Main Program For CarBot
+ Including IR sensor, Collision sensor, Motor, Servo
+ 
+ Copyright @ InfiniteSense.com
+*/
+
+#include <Servo.h> 
+
+/* ir pin */
+#define PIN_IR1_LEFT A0
+#define PIN_IR2_LEFT A1
+#define PIN_IR3_LEFT A2
+#define PIN_IR3_RIGHT A3
+#define PIN_IR2_RIGHT A4
+#define PIN_IR1_RIGHT A5
+
+/* collision pin */
+#define PIN_COLLISION_IR 3
+
+/* servo pin */
+#define PIN_SERVO 2
+
+#define IR_NUM 6
+#define IR_NUM_MAX 6
+
+#define STATE_TO_TOWER 0
+#define STATE_TO_HOME 1
+#define STATE_POST_BALL 2
+#define STATE_FINDING_LINE 3
+
+
+int collision_ir = 0;
+int ir_array[IR_NUM] = { 0 };
+const int ir_pin_array[IR_NUM_MAX] = {
+   PIN_IR1_LEFT, PIN_IR2_LEFT, PIN_IR3_LEFT, PIN_IR3_RIGHT, PIN_IR2_RIGHT, PIN_IR1_RIGHT
+};
+const int ir_pos_map[IR_NUM_MAX] = {
+    -5, -3, -1, 1, 3, 5
+};
+
+int car_direction = STATE_TO_TOWER;
+Servo myservo;
+
+void setup()
+{
+  initSerial();
+  initMotor();
+  initIR();
+  initServo();
+  delay(1000);
+}
+
+void loop() 
+{ 
+  readIR();
+  process();
+  driveMotor();
+}
+
+void initServo()
+{
+  myservo.attach(PIN_SERVO);
+  myservo.write(90);
+}
+
+void initSerial()
+{
+  Serial.begin(9600);
+}
+
+void process()
+{
+  int i;
+  if (collision_ir == LOW) {
+    if (car_direction == STATE_TO_TOWER) {
+      car_direction = STATE_FINDING_LINE;
+      stopDead(1000);
+      driveMotor();
+      delay(1000);
+      myservo.write(45);
+      delay(1000);
+      runLeft(180, 2);
+      for (i = 0; i < 6000; i++) {
+        driveMotor();
+      }
+    } else if (car_direction == STATE_TO_HOME) {
+      stopDead(1000);
+    }
+    return;
+  }
+  if (car_direction == STATE_TO_TOWER) {
+      // ir 5
+      if (ir_array[0] == LOW && ir_array[2] == LOW) {
+        runLeft(150, 2);
+      } else if (ir_array[1] == LOW && ir_array[2] == LOW) {
+        runLeft(120, 2);
+      } else if (ir_array[2] == LOW) {
+        runForward(150, 2);
+      } else if (ir_array[1] == LOW) { 
+        runLeft(120, 2);
+      } else if (ir_array[3] == LOW) {
+        runRight(120, 2);
+      } else if (ir_array[0] == LOW) {
+        runLeft(150 ,2);
+      } else if (ir_array[4] == LOW) {
+        runRight(150, 2);
+      }
+    } else if (car_direction == STATE_TO_HOME) {
+      // ir 5
+      if (ir_array[4] == LOW && ir_array[3] == LOW && ir_array[2] == LOW) {
+        runRight(180, 2);
+        // specific process
+        for (i = 0; i < 6000; i++) {
+          driveMotor();
+        }
+      } else if (ir_array[4] == LOW && ir_array[2] == LOW) {
+        runRight(180, 2);
+      } else if (ir_array[3] == LOW && ir_array[2] == LOW) {
+        runRight(150, 2);
+      } else if (ir_array[2] == LOW) {
+        runByPosition(0, 2);
+      } else if (ir_array[1] == LOW) {
+        runByPosition(4, 2);
+      } else if (ir_array[3] == LOW) {
+        runByPosition(-4, 2);
+      } else if (ir_array[0] == LOW) {
+        runByPosition(6, 2);
+      } else if (ir_array[4] == LOW) {
+        runByPosition(-6, 2);
+      }
+    } else if (car_direction == STATE_FINDING_LINE) {
+      if (ir_array[0] == HIGH && ir_array[1] == HIGH && ir_array[2] == HIGH && ir_array[3] == HIGH && ir_array[4] == HIGH) {
+        runLeft(120, 2);
+      } else {
+        car_direction = STATE_TO_HOME;
+      }
+    }
+}
+
+/*
+ IR Sensor
+ */
+
+void initIR()
+{
+  int i;
+  // setting input pins
+  for (i = 0; i < IR_NUM_MAX; i++) {
+    pinMode(ir_pin_array[i], INPUT);
+  }
+  pinMode(PIN_COLLISION_IR, INPUT);
+}
+
+int readIR()
+{
+  int i;
+  for (i = 0; i < IR_NUM; i++) {
+     ir_array[i] = digitalRead(ir_pin_array[i]);
+  }
+  collision_ir = digitalRead(PIN_COLLISION_IR);
+}
